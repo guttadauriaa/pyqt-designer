e75100ef96f1b50d71250c68dda7598480cece3b
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2026-02-24 12:24:15 +0100

    Fix exemples/imshow.py bloqué avec fermeture de la fenêtre.

--- exemples/imshow.py
+++ exemples/imshow.py
@@ -1,7 +1,22 @@
 import cv2
 
 img = cv2.imread('exemples/dog_bike_car.jpg')
-cv2.imshow('Dog Bike & Car', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+win_name = 'Dog Bike & Car'
+if img is not None:
+    cv2.imshow(win_name, img)
+
+    while True:
+        key = cv2.waitKey(100) & 0xFF
+
+        # if key is Escape or 'q', we break
+        if key == 27 or key == ord('q'):
+            break
+
+        # if window is closed, WND_PROP_VISIBLE will be False (0)
+        if cv2.getWindowProperty(win_name, cv2.WND_PROP_VISIBLE) < 1:
+            break
+
+    cv2.destroyAllWindows()
+else:
+    print("Image non trouvée.")
 
