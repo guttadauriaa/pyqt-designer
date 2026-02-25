0b56f82629f3af48ba2d180e653a649ef70d0f65
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2026-02-25 11:21:43 +0100

    Fix Arrêt du programme à la fermeture de la fenêtre.
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

--- exemples/webcam-imshow.py
+++ exemples/webcam-imshow.py
@@ -2,6 +2,7 @@ import cv2
 
 # Ouvrir la webcam (0 = caméra par défaut)
 cap = cv2.VideoCapture(0)
+win_name = "Webcam (Press 'q' to quit.)"
 
 if not cap.isOpened():
     print("Erreur : Impossible d'accéder à la webcam.")
@@ -12,8 +13,10 @@ else:
         if not ret:
             print("Erreur : Impossible de lire le flux vidéo.")
             break
-        cv2.imshow("Webcam (Press 'q' to quit.)", frame)
+        cv2.imshow(win_name, frame)
         if cv2.waitKey(1) == ord('q'):  # Appuyez sur 'q' pour quitter
+            break
+        if cv2.getWindowProperty(win_name, cv2.WND_PROP_VISIBLE) < 1:
             break
 
 cap.release()
